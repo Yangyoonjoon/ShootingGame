26dde850b38e32136e966f23e6b8a1b47cf0a784
Yangyoonjoon <64205127+Yangyoonjoon@users.noreply.github.com>
2020-10-25 13:14:05 +0900

    Big Update
    총, 궁극기 구현
    hp 회복 아이템 구현
    점수 제도 도입

--- game.py
+++ game.py
@@ -7,19 +7,26 @@ from PyQt5.QtWidgets import QInputDialog
 
 # 적군 클래스
 class Enemy:
-    def __init__(self, r, d, s):
+    def __init__(self, r, d, s, c, t):
         self.r = r
         # 방향 0:Left, 1:Up, 2:Right, 3:Down
         self.dir = d
         self.size = s
         self.die = False
-        self.col =  QColor(255,0,0)
-        self.type = 0
+        self.col =  c
+        # 타입 0:적군, 1:hp회복
+        self.type = t
 
 class Mine:
     def __init__(self, r):
         self.r = r
         self.die = False
+
+class Bullet:
+    def __init__(self, r, d):
+        self.r = r
+        self.die = False
+        self.dir = d
 
 class Game(QObject):
 
@@ -33,36 +40,49 @@ class Game(QObject):
         self.parent = w
         self.rect = w.rect()
 
+        # 점수
+        self.score = 0
+
+        # 에너지
+        self.energy = 0
+
+        # 궁극기
+        self.ultimate = False
+
+        # 궁극기 효과 지속 시간
+        self.ultimateC = True
+        self.ultimateCt = 500
+
         # 아군
         pt = self.rect.center()
         size = 30
         self.my = QRectF(pt.x()-size/2, pt.y()-size/2, size, size)
         self.hp = 10
 
-        # 게임 난이도 조절
-        label = ('상', '중', '하')
-        item = QInputDialog.getItem(self.parent, '게임 시작', '난이도 설정', label, 0, False)
-
-        if item[1]:
-            pass
-        else:
-            self.hp = 0
-
         # 지뢰
         self.mine = []
 
         # 지뢰 쿨타임
-        self.c = True
-        self.ct = 300
+        self.mineC = True
+        self.mineCt = 300
+
+        # 총알
+        self.bullet = []
 
         # 적군
         self.e = []
+        # 적군 속도
+        self.esp = 2
 
         # 이동
         self.L = False
         self.R = False
         self.U = False
         self.D = False
+
+        # 마지막으로 누른 이동 버튼
+        # 방향 0:Left, 1:Up, 2:Right, 3:Down
+        self.key = 1
 
         # 시그널 처리
         self.update_signal.connect(self.parent.update)
@@ -92,7 +112,7 @@ class Game(QObject):
         qp.drawRect(self.my)
         p = QPen(QColor(255,255,255), 1, Qt.SolidLine)
         qp.setPen(p)
-        f = QFont('궁서체', 15)
+        f = QFont('돋움체', 15)
         f.setBold(True)
         qp.setFont(f)
         qp.drawText(self.my, Qt.AlignCenter, str(self.hp))
@@ -103,32 +123,67 @@ class Game(QObject):
         for m in self.mine:
             qp.drawEllipse(m.r)
 
-        # 지뢰 쿨타임 표시
+        # 점수 & 지뢰 쿨타임 & 총알 개수 표시
         p = QPen(QColor(0,0,0), 1, Qt.SolidLine)
         qp.setPen(p)
-        f = format(self.ct/100, '0.2f')
-        txt = f'쿨타임:{f}초'
+        f = format(self.mineCt/100, '0.2f')
+        txt = f'점수:{self.score} \n쿨타임:{f}초 \n총알 개수:{10 - len(self.bullet)}'
         qp.drawText(self.rect, Qt.AlignTop|Qt.AlignLeft, txt)
+
+        # 에너지 & 궁극기 남은 시간 표시
+        if self.ultimateC == False:
+            f = format(self.ultimateCt/100, '0.2f')
+            txt = f'궁극기 남은 시간:{f}'
+        else:
+            txt = f'에너지:{self.energy}/50'
+        qp.drawText(self.rect, Qt.AlignTop|Qt.AlignRight, txt)
+
+        # 총알 그리기
+        # 궁극기 사용 가능시 총알 색상이 노란색으로 변함
+        if self.energy >= 50:
+            b = QBrush(QColor(255, 255, 0))
+            qp.setBrush(b)
+        else:
+            b = QBrush(QColor(0, 0, 0))
+            qp.setBrush(b)
+        for bullet in self.bullet:
+            qp.drawEllipse(bullet.r)
 
     def keyDown(self, key):
         if key == Qt.Key_Left or key == Qt.Key_A:
             self.L = True
+            self.key = 0
         elif key == Qt.Key_Right or key == Qt.Key_D:
             self.R = True
+            self.key = 2
         elif key == Qt.Key_Up or key == Qt.Key_W:
             self.U = True
+            self.key = 1
         elif key == Qt.Key_Down or key == Qt.Key_S:
             self.D = True
+            self.key = 3
                         
         # 지뢰 생성
-        if key == Qt.Key_Space and self.c:
+        if (key == Qt.Key_H or key == Qt.Key_K) and self.mineC:
             size = 100
             pt = self.my.center()
             x = pt.x() - size/2
             y = pt.y() - size/2
             rect = QRectF(x, y, size, size)
             self.mine.append(Mine(rect))
-            self.c = False
+            self.mineC = False
+
+        # 총알 생성
+        if key == Qt.Key_J and len(self.bullet) <= 10:
+            size = 5
+            pt = self.my.center()
+            rect = QRectF(pt.x(), pt.y(), size, size)
+            dir = self.key
+            self.bullet.append(Bullet(rect, dir))
+
+        # 궁극기 사용
+        if key == Qt.Key_Space:
+            self.ultimate = True
 
     def keyUp(self, key):
         if key == Qt.Key_Left or key == Qt.Key_A:
@@ -141,13 +196,18 @@ class Game(QObject):
             self.D = False
 
     def threadFunc(self):
-        sp = 4
-        esp = 2
-        gap = 5
-        # 적군 사이즈
-        esize = 30
-
         while self.bRun:
+            # 아군 속도
+            sp = 4
+            # 적군 속도
+            esp = self.esp
+            #총알 속도
+            bsp = 5
+            # 적군 사이즈
+            esize = 30
+
+            gap = 5
+
             # 아군 이동 처리
             if self.L and self.my.left() > self.rect.left() + gap:
                 self.my.adjust(-sp, 0, -sp, 0)
@@ -176,22 +236,74 @@ class Game(QObject):
                     x = random.randint(0, self.rect.width() - esize)
                     y = self.rect.bottom()
 
+                t = random.randint(1, 100)
+                if t <= 3:
+                    t = 1
+                    c = QColor(0,0,255)
+                else:
+                    t = 0
+                    c = QColor(255,0,0)
                 rect = QRectF(x, y, esize, esize)
-                e = Enemy(rect, d , esize)
+                e = Enemy(rect, d , esize, c, t)
 
                 self.e.append(e)
 
-            # 적군 이동 처리 & 삭제
+            # 총알 이동
+            # 방향 0:Left, 1:Up, 2:Right, 3:Down
+            for b in self.bullet:
+                if b.dir == 0:
+                    if b.r.right() < self.rect.left():
+                        b.die = True
+                    else:
+                        b.r.adjust(-bsp, 0, -bsp, 0)
+                elif b.dir == 1:
+                    if b.r.bottom() < self.rect.top():
+                        b.die = True
+                    else:
+                        b.r.adjust(0, -bsp, 0, -bsp)
+                elif b.dir == 2:
+                    if b.r.left() > self.rect.right():
+                        b.die = True
+                    else:
+                        b.r.adjust(bsp, 0, bsp, 0)
+                else:
+                    if b.r.top() > self.rect.bottom():
+                        b.die = True
+                    else:
+                        b.r.adjust(0, bsp, 0, bsp)
+
+            # 궁극기
+            if self.energy >= 50 and self.ultimate == True:
+                self.energy -= 50
+                self.esp = 0.5
+                self.ultimateC = False
+                self.ultimate = False
+
+            # 궁극기 효과 지속
+            if self.ultimateC == False:
+                self.ultimateCt -= 1
+
+            if self.ultimateCt <= 0:
+                self.ultimateC = True
+                self.ultimateCt = 500
+                self.esp = 2
+
+            # 적군의 이동 및 충돌 처리
             for e in self.e:
                 # 적군과 나의 충돌
                 if self.my.intersects(e.r):
                     e.die = True
-                    self.hp -= 1
+                    if e.type == 0:
+                        self.hp -= 1
+                    else:
+                        if self.hp < 10:
+                            self.hp += 1
                     continue
 
+                # 지뢰와 적군의 충돌
                 bNext = False
                 for m in self.mine:
-                    if m.r.intersects(e.r):
+                    if m.r.intersects(e.r) and e.type == 0:
                         m.die = True
                         e.die = True
                         bNext = True
@@ -200,6 +312,16 @@ class Game(QObject):
                 if bNext:
                     continue
 
+                # 총알과 적군의 충돌
+                for bullet in self.bullet:
+                    if bullet.r.intersects(e.r) and e.type == 0:
+                        bullet.die = True
+                        e.die = True
+                        self.score += 1
+                        if self.energy < 50 and self.ultimateC == True:
+                            self.energy += 1
+
+                # 적군 이동 처리 및 벽과 충돌 처리
                 # 방향 0:Left, 1:Up, 2:Right, 3:Down
                 if e.dir == 0:
                     if e.r.left() > self.rect.right():
@@ -230,18 +352,20 @@ class Game(QObject):
             self.mine = [m for m in self.mine if m.die == False]
 
             # 지뢰 쿨타임
-            if self.c == False:
-                self.ct -= 1
+            if self.mineC == False:
+                self.mineCt -= 1
 
-            if self.ct <= 0:
-                self.c = True
-                self.ct = 300
+            if self.mineCt <= 0:
+                self.mineC = True
+                self.mineCt = 300
+
+            # 총알 삭제
+            self.bullet = [bullet for bullet in self.bullet if bullet.die == False]
 
             # 게임 종료 (hp <= 0)
             if self.hp <= 0:
                 self.gameover_signal.emit()
                 break
-
 
             time.sleep(0.01)
             self.update_signal.emit()
